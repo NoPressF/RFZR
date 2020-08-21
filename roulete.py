975cc1f588d265bced87ec8c809d100814c4080b
NoPressF <vovaefanov478@gmail.com>
2020-08-21 22:46:51 +0300

    first commit

---
+++ roulete.py
@@ -0,0 +1,462 @@
+import random
+import time
+import sys
+import os
+# ФУНКЦИИ
+def select():
+    print(random.choice(listl))
+# Start
+def start():
+    random = input("")
+    if random == "Старт":
+        times()
+        timestart()
+# Timer
+def times():
+    print(starttimer)
+# TimeStart
+def timestart():
+    time.sleep(1)
+    print(time1)
+    random.shuffle(listl)
+    time.sleep(2)
+    print(time2)
+    random.shuffle(listl)
+    time.sleep(2)
+    print(time3)
+    random.shuffle(listl)
+    time.sleep(1)
+    print(time4)
+    random.shuffle(listl)
+    time.sleep(2)
+    print(time5)
+    random.shuffle(listl)
+    time.sleep(2)
+    print(time6)
+    random.shuffle(listl)
+
+def selcho ():
+    category = input("")
+    if category == "5":
+        print(entername)
+        name = input("")
+        print(true)
+        if name not in listl:
+          listl.append(name)
+          print(entername_ag)
+          name = input("")
+          print(true)
+        if name not in listl:
+          listl.append(name)
+          print(entername_ag)
+          name = input("")
+          print(true)
+        if name not in listl:
+          listl.append(name)
+          print(entername_ag)
+          name = input("")
+          print(true)
+        if name not in listl:
+          listl.append(name)
+          print(entername_ag)
+          name = input("")
+          print(true)
+        if name not in listl:
+          listl.append(name)
+          print(choose)
+          # Countdown
+          start()
+          # Result
+          select + print(zombie)()
+          sys.exit()
+    if category == "6": 
+          print(entername)
+          name2 = input("")
+          print(true)
+          if name2 not in listl:
+           listl.append(name2)
+           print(entername_ag)
+           name2 = input("")
+           print(true)
+          if name2 not in listl:
+           listl.append(name2)
+           print(entername_ag)
+           name2 = input("")
+           print(true)
+          if name2 not in listl:
+           listl.append(name2)
+           print(entername_ag)
+           name2 = input("")
+           print(true)
+          if name2 not in listl:
+           listl.append(name2)
+           print(entername_ag)
+           name2 = input("")
+           print(true)
+          if name2 not in listl:
+           listl.append(name2)
+           print(entername_ag)
+           name2 = input("")
+          if name2 not in listl:
+           listl.append(name2)
+           print(choose)
+           # Countdown
+           start()
+           # Result
+           select()
+           sys.exit()
+    if category == "7": 
+        print(entername)
+        name3 = input("")
+        print(true)
+        if name3 not in listl:
+         listl.append(name3)
+         print(entername_ag)
+         name3 = input("")
+         print(true)
+        if name3 not in listl:
+         listl.append(name3)
+         print(entername_ag)
+         name3 = input("")
+         print(true)
+        if name3 not in listl:
+         listl.append(name3)
+         print(entername_ag)
+         name3 = input("")
+         print(true)
+        if name3 not in listl:
+         listl.append(name3)
+         print(entername_ag)
+         name3 = input("")
+         print(true)
+        if name3 not in listl:
+         listl.append(name3)
+         print(entername_ag)
+         name3 = input("")
+         print(true)
+        if name3 not in listl:
+         listl.append(name3)
+         print(entername_ag)
+         name3 = input("")
+         print(true)
+        if name3 not in listl:
+         listl.append(name3)
+         print(choose)
+         # Countdown
+         start()
+         # Result
+         select()
+         sys.exit()
+    if category == "8":
+        print(entername)
+        name4 = input("")
+        if name4 not in listl:
+         listl.append(name4)
+         print(entername_ag)
+         name4 = input("")
+         print(true)
+        if name4 not in listl:
+         listl.append(name4)
+         print(entername_ag)
+         name4 = input("")
+         print(true)
+        if name4 not in listl:
+         listl.append(name4)
+         print(entername_ag)
+         name4 = input("")
+         print(true)
+        if name4 not in listl:
+         listl.append(name4)
+         print(entername_ag)
+         name4 = input("")
+         print(true)
+        if name4 not in listl:
+         listl.append(name4)
+         print(entername_ag)
+         name4 = input("")
+         print(true)
+        if name4 not in listl:
+         listl.append(name4)
+         print(entername_ag)
+         name4 = input("")
+         print(true)
+        if name4 not in listl:
+         listl.append(name4)
+         print(entername_ag)
+         name4 = input("")
+         print(true)
+        if name4 not in listl:
+         listl.append(name4)
+         print(choose)
+         # Countdown
+         start()
+         # Result
+         select()
+         sys.exit()
+    if category == "9":
+        print(entername)
+        name5 = input("")
+        if name5 not in listl:
+         listl.append(name5)
+         print(entername_ag)
+         name5 = input("")
+         print(true)
+        if name5 not in listl:
+         listl.append(name5)
+         print(entername_ag)
+         name5 = input("")
+         print(true)
+        if name5 not in listl:
+         listl.append(name5)
+         print(entername_ag)
+         name5 = input("")
+         print(true)
+        if name5 not in listl:
+         listl.append(name5)
+         print(entername_ag)
+         name5 = input("")
+         print(true)
+        if name5 not in listl:
+         listl.append(name5)
+         print(entername_ag)
+         name5 = input("")
+         print(true)
+        if name5 not in listl:
+         listl.append(name5)
+         print(entername_ag)
+         name5 = input("")
+         print(true)
+        if name5 not in listl:
+         listl.append(name5)
+         print(entername_ag)
+         name5 = input("")
+         print(true)
+         print(entername_ag)
+        if name4 not in listl:
+         listl.append(name5)
+         print(entername_ag)
+         name5 = input("")
+         print(true)
+        if name5 not in listl:
+         listl.append(name5)
+         print(choose)
+         # Countdown
+         start()
+         # Result
+         select()
+         sys.exit()
+    if category == "10":
+        print(entername)
+        name6 = input("")
+        if name6 not in listl:
+         listl.append(name6)
+         print(entername_ag)
+         name6 = input("")
+         print(true)
+        if name6 not in listl:
+         listl.append(name6)
+         print(entername_ag)
+         name6 = input("")
+         print(true)
+        if name6 not in listl:
+         listl.append(name6)
+         print(entername_ag)
+         name6 = input("")
+         print(true)
+        if name6 not in listl:
+         listl.append(name6)
+         print(entername_ag)
+         name6 = input("")
+         print(true)
+        if name6 not in listl:
+         listl.append(name6)
+         print(entername_ag)
+         name6 = input("")
+         print(true)
+        if name6 not in listl:
+         listl.append(name6)
+         print(entername_ag)
+         name6 = input("")
+         print(true)
+        if name6 not in listl:
+         listl.append(name6)
+         print(entername_ag)
+         name6 = input("")
+         print(true)
+        if name6 not in listl:
+         listl.append(name6)
+         print(entername_ag)
+         name6 = input("")
+         print(true)
+        if name6 not in listl:
+         listl.append(name6)
+         print(entername_ag)
+         name6 = input("")
+         print(true)
+        if name6 not in listl:
+         listl.append(name6)
+         print(choose)
+         # Countdown
+         start()
+         # Result
+         select()
+         sys.exit()
+    if category == "11":
+        print(entername)
+        name7 = input("")
+        print(true)
+        if name7 not in listl:
+         listl.append(name7)
+         print(entername_ag)
+         name7 = input("")
+         print(true)
+        if name7 not in listl:
+         listl.append(name7)
+         print(entername_ag)
+         name7 = input("")
+         print(true)
+        if name7 not in listl:
+         listl.append(name7)
+         print(entername_ag)
+         name7 = input("")
+         print(true)
+        if name7 not in listl:
+         listl.append(name7)
+         print(entername_ag)
+         name7 = input("")
+         print(true)
+        if name7 not in listl:
+         listl.append(name7)
+         print(entername_ag)
+         name7 = input("")
+         print(true)
+        if name7 not in listl:
+         listl.append(name7)
+         print(entername_ag)
+         name7 = input("")
+         print(true)
+        if name7 not in listl:
+         listl.append(name7)
+         print(entername_ag)
+         name7 = input("")
+         print(true)
+        if name7 not in listl:
+         listl.append(name7)
+         print(entername_ag)
+         name7 = input("")
+         print(true)
+        if name7 not in listl:
+         listl.append(name7)
+         print(entername_ag)
+         name7 = input("")
+         print(true)
+        if name7 not in listl:
+         listl.append(name7)
+         print(entername_ag)
+         name7 = input("")
+         print(true)
+        if name7 not in listl:
+         listl.append(name7)
+         print(choose)
+         # Countdown
+         start()
+         # Result
+         select()
+         sys.exit()
+    if category == "12":
+        print(entername)
+        name8 = input("")
+        print(true)
+        if name8 not in listl:
+         listl.append(name8)
+         print(entername_ag)
+         name8 = input("")
+         print(true)
+        if name8 not in listl:
+         listl.append(name8)
+         print(entername_ag)
+         name8 = input("")
+         print(true)
+        if name8 not in listl:
+         listl.append(name8)
+         print(entername_ag)
+         name8 = input("")
+         print(true)
+        if name8 not in listl:
+         listl.append(name8)
+         print(entername_ag)
+         name8 = input("")
+         print(true)
+        if name8 not in listl:
+         listl.append(name8)
+         print(entername_ag)
+         name8 = input("")
+         print(true)
+        if name8 not in listl:
+         listl.append(name8)
+         print(entername_ag)
+         name8 = input("")
+         print(true)
+        if name8 not in listl:
+         listl.append(name8)
+         print(entername_ag)
+         name8 = input("")
+         print(true)
+        if name8 not in listl:
+         listl.append(name8)
+         print(entername_ag)
+         name8 = input("")
+         print(true)
+        if name8 not in listl:
+         listl.append(name8)
+         print(entername_ag)
+         name8 = input("")
+         print(true)
+        if name8 not in listl:
+         listl.append(name8)
+         print(entername_ag)
+         name8 = input("")
+         print(true)
+        if name8 not in listl:
+         listl.append(name8)
+         print(entername_ag)
+         name8 = input("")
+         print(true)
+        if name8 not in listl:
+         listl.append(name8)
+         print(entername_ag)
+         print(choose)
+         # Countdown
+         start()
+         # Result
+         select()
+         sys.exit()
+    else:
+      if not category == "5" or "6" or "7" or "8" or "9" or "10" or "11" or "12":
+        print(error)
+        sys.exit()
+# Vars
+error = "Написанного вами цифры/слова не существует в списке!"
+catg = "Выберите число игроков (5) (6) (7) (8) (9) (10) (11) (12)"
+enpl = "Введите число игроков будут участвовать в рандоме!"
+zombie = "Зомби становится:  "
+true = "Имя успешно добавлено!"
+listl = []
+welcome = "Добро пожаловать в рандомный выбор Зомби!"
+choose = "Введите команду (Старт)"
+entername = "Введите имя которое будет добавлено в рандом!"
+entername_ag = "Введите повторно имя которое будет добавлено в рандом!"
+starttimer = "Засечено 10 секунд на подбор Зомби!"
+time1 = "Подбор.."
+time2 = "Подбор..."
+time3 = "Подбор зомби...."
+time4 = "Ещё чуть-чуть...."
+time5 = "Перемешиваем....."
+time6 = "Высчитываем результат....."
+# Start roulete
+print("-" * 45)
+print(welcome)
+print("-" * 45)
+print(enpl)
+print(catg)
+selcho()
